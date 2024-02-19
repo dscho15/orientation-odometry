c559c89cb12256accd9c6e6f7861d9fa9d0633c5
dts <dts@esoft.com>
2024-02-19 17:01:04 +0000

    cleand dataset

--- dataset.py
+++ dataset.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pandas as pd
 from PIL import Image
 from pprint import pprint
-import webbrowser
 
 
 def read_calib_kitty(calib_path: Path) -> list[np.ndarray]:
@@ -37,7 +36,6 @@ class KittyDataset:
         self,
         p_imgs: str,
         calib_path: str = None,
-        timestamps: str = None,
         grayscale: bool = False,
     ) -> None:
         assert Path(p_imgs).exists(), "Dataset path does not exist"
@@ -45,14 +43,9 @@ class KittyDataset:
         self.calib_data = read_calib_kitty(calib_path)
         self.camera_matrix = self.calib_data[f"K_0{self.p_imgs[-1]}:"]
         self.idx = int(p_imgs.split("_")[-1])
-        self.timestamps = pd.read_csv(timestamps, header=None, names=["timestamp"])
         self.imgs = Path(self.p_imgs).rglob("*.png")
         self.imgs = sorted([str(img) for img in self.imgs])
         self.grayscale = grayscale
-
-    @property
-    def get_timestamps(self) -> pd.DataFrame:
-        return self.timestamps
 
     @property
     def get_raw_extrinsics(self) -> np.ndarray:
@@ -80,19 +73,42 @@ class CustomDataset:
     def __init__(
         self,
         p_imgs: str,
-        pinhole_params: list[float] = [2534, 2534, 1920 // 2, 1080 // 2],
-        grayscale: bool = False,
+        pinhole_params: list[float] = None,
+        fov_w_deg: float = 65,
+        grayscale: bool = True,
+        max_img_size: int = 1024,
+        n_imgs: int = None,
     ) -> None:
         assert Path(p_imgs).exists(), "Dataset path does not exist"
-        assert len(pinhole_params) == 4, "Pinhole parameters must be of length 4"
         self.p_imgs = p_imgs
-        
-        self.camera_matrix = np.ones((3, 3))
-        self.camera_matrix[0, 0], self.camera_matrix[1, 1] = pinhole_params[:2]
-        self.camera_matrix[0, 2], self.camera_matrix[1, 2] = pinhole_params[2:]
 
-        self.imgs = Path(self.p_imgs).rglob("*.png")
+        self.imgs = Path(self.p_imgs).rglob("*.jpg")
         self.imgs = sorted([str(img) for img in self.imgs])
+        if n_imgs is not None:
+            self.imgs = self.imgs[:n_imgs]
+        
+        self.max_img_size = max_img_size
+        self.camera_matrix = np.ones((3, 3))
+        if pinhole_params is not None:
+            self.camera_matrix[0, 0], self.camera_matrix[1, 1] = pinhole_params[:2]
+            self.camera_matrix[0, 2], self.camera_matrix[1, 2] = pinhole_params[2:]
+        elif fov_w_deg is not None:
+            self.field_of_view = fov_w_deg
+            
+            w, h = self.get_hw
+            
+            if max_img_size < max(w, h):
+                if h > w:
+                    w = int(w * max_img_size / h)
+                    h = max_img_size
+                else:
+                    h = int(h * max_img_size / w)
+                    w = max_img_size
+                
+            self.camera_matrix[0, 0] = w / (2 * np.tan(np.radians(fov_w_deg) / 2))
+            self.camera_matrix[1, 1] = w / (2 * np.tan(np.radians(fov_w_deg) / 2))
+            self.camera_matrix[0, 2] = w / 2
+            self.camera_matrix[1, 2] = h / 2
         
         self.grayscale = grayscale
 
@@ -105,24 +121,34 @@ class CustomDataset:
     @property
     def get_hw(self) -> tuple:
         img = Image.open(self.imgs[0])
-        h, w = img.size
+        w, h = img.size
         return h, w
 
     def __len__(self) -> int:
         return len(self.imgs)
+    
+    def resize(self, img: np.ndarray) -> np.ndarray:
+        (h, w) = img.shape[:2]
+        
+        if self.max_img_size > max(w, h):
+            return img
+        
+        if h > w:
+            w = int(w * 1024 / h)
+            h = 1024
+        else:
+            h = int(h * 1024 / w)
+            w = 1024
+            
+        img = Image.fromarray(img)
+        img = img.resize((w, h))
+        
+        return np.array(img)
 
     def __getitem__(self, idx):
         img = Image.open(self.imgs[idx])
+        
         if self.grayscale:
             img = img.convert("L")
-        img = np.array(img)
-        return img
-
-
-if __name__ == "__main__":
-    kitty_dataset = KittyDataset(
-        "datasets/2011_09_26/2011_09_26_drive_0018_extract/image_02",
-        "datasets/2011_09_26/calib_cam_to_cam.txt",
-        "datasets/2011_09_26/2011_09_26_drive_0018_extract/image_00/timestamps.txt",
-    )
-    webbrowser.open(kitty_dataset.imgs[0])
+            
+        return self.resize(np.array(img))
